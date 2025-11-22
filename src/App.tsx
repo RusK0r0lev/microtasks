0cd06d262e033eb93de016775361f212add19191
RusKorolev <rskorolev@bk.ru>
2025-11-22 21:08:57 +0700

    хук useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,44 @@ import './App.css';
 // import Header from './site/Header';
 // import Body from './site/Body';
 // import Footer from './site/Footer';
-import NewComponent from './NewComponent';
+// import NewComponent from './NewComponent';
 import { useState } from 'react';
+
 function App() {
-    const [students, setStudents] = useState([
-        { id: 1, name: 'James', age: 8 },
-        { id: 2, name: 'Robert', age: 18 },
-        { id: 3, name: 'John', age: 28 },
-        { id: 4, name: 'Michael', age: 38 },
-        { id: 5, name: 'William', age: 48 },
-        { id: 6, name: 'David', age: 58 },
-        { id: 7, name: 'Richard', age: 68 },
-        { id: 8, name: 'Joseph', age: 78 },
-        { id: 9, name: 'Thomas', age: 88 },
-        { id: 10, name: 'Charles', age: 98 },
-        { id: 11, name: 'Christopher', age: 100 },
-    ]);
+    // const [students, setStudents] = useState([
+    //     { id: 1, name: 'James', age: 8 },
+    //     { id: 2, name: 'Robert', age: 18 },
+    //     { id: 3, name: 'John', age: 28 },
+    //     { id: 4, name: 'Michael', age: 38 },
+    //     { id: 5, name: 'William', age: 48 },
+    //     { id: 6, name: 'David', age: 58 },
+    //     { id: 7, name: 'Richard', age: 68 },
+    //     { id: 8, name: 'Joseph', age: 78 },
+    //     { id: 9, name: 'Thomas', age: 88 },
+    //     { id: 10, name: 'Charles', age: 98 },
+    //     { id: 11, name: 'Christopher', age: 100 },
+    // ]);
+
+    const [count, setCount] = useState(0);
+    function onClickHandler() {
+        setCount(count + 1);
+    }
+    function onClickReset() {
+        setCount(0);
+    }
+
     return (
         <>
             {/* <h3>Привет Микротаски</h3>
             <Header title={'New body'} />
             <Body title={'Привет Боди'} />
-            <Footer title={'Привет футер'} /> */}
-            <NewComponent students={students} />
+            <Footer title={'Привет футер'} /> 
+            <NewComponent students={students} />*/}
+            <h1>{count}</h1>
+            <button onClick={onClickHandler}>
+                Прибавить <b>1</b>
+            </button>
+            <button onClick={onClickReset}>Обнулить</button>
         </>
     );
 }
